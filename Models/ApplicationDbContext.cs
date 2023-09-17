5f1a041c4f2d133f5c00fa1587fbe38e56386d70
MohamedHamdy9 <124048770+MohamedHamdy9@users.noreply.github.com>
2023-09-17 21:04:24 +0200

    added

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DotNETMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
